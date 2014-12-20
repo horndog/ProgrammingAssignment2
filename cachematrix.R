9421fa423f0ed6e927b8dd1c84235ce80e52fff6
horndog <hornd@tcd.ie>
2014-12-20 17:48:31 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,27 +3,25 @@
 makeCacheMatrix <- function(x = matrix()) {
   
   # Holds the cached value or NULL if nothing is cached
-  m<-NULL
+  m <- NULL
   
   # Store a matrix
-  set<-function(y){
-    x<<-y
-    m<<-NULL
+  set <- function(y){
+    x <<- y
+    m <<- NULL
   }
   
   # Return the matrix
-  get<-function() x
+  get <- function() x
   
   # Cache the matrix
-  setmatrix<-function(solve) m <<- solve
+  setmatrix <- function(solve) m <<- solve
   
   # Get the cached value
-  getmatrix<-function() m
+  getmatrix <- function() m
   
   # Return a list of the following elements
-  list(set=set, get=get,
-       setmatrix=setmatrix,
-       getmatrix=getmatrix)
+  list(set=set, get=get, setmatrix=setmatrix, getmatrix=getmatrix)
 
 }
 
@@ -32,7 +30,7 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x=matrix(), ...) {
   
   # Get the cached value
-  m<-x$getmatrix()
+  m <- x$getmatrix()
   
   # If it exists return the value
   if(!is.null(m)){
